8856acd13e578127dab409c73cd88f67ad196f54
Dandan Li <evelyn.green.lee@gmail.com>
2022-10-05 14:40:59 -0400

    get users,user,still error

--- src/components/CookieClicker.js
+++ src/components/CookieClicker.js
@@ -2,7 +2,7 @@ import React, { useContext,useState, useEffect } from "react";
 import { AuthContext } from "../context/AuthContext";
 // import { onAuthStateChanged } from "firebase/auth";
 import { ref } from "firebase/storage";
-import { doc, setDoc } from "firebase/firestore"; 
+import { doc, setDoc, getDoc, collection, getDocs } from "firebase/firestore"; 
 import { db, storage } from '../server/firebase';
 import {onDisconnect} from "firebase/database";
 
@@ -10,6 +10,10 @@ export const CookieClicker = () => {
 
     const {currentUser} = useContext(AuthContext)
     const [player, setPlayer] = useState({})
+    const [playerId,setPlayerId] = useState("");
+
+    let playersCollectionRef = collection(db,"player");
+    
 
     useEffect(()=>{
         const addPlayer = async()=>{
@@ -19,24 +23,43 @@ export const CookieClicker = () => {
                 points:0
             })
             setPlayer(player);
-    
+            setPlayerId(currentUser.uid);
+        };
+        addPlayer();
+
+        //to get all players in "player" collection
+        const getUsers = async()=>{
+            const data = await getDocs(playersCollectionRef);
+            console.log("users",data);
+            let currentPlayerRef = doc(db,"player",playerId);
+            let currentPlayerSnap = await getDoc(currentPlayerRef);
+            console.log("currentPlayer",currentPlayerSnap);
         }
-    },[])
+        getUsers();
+
+    
+        // let playerRef = ref(storage,`player/${currentUser.uid}`);
+
+        // console.log("playerRef",playerRef);
+        // onDisconnect(playerRef).remove().catch((err) => {
+        //     if (err) {
+        //       console.error("could not establish onDisconnect event", err);
+        //     }
+        //   });
+    },[player,playerId,currentUser])
 
 
     // addPlayer();
     // let playerRef = storage.ref("player");
 
-    let playerRef = ref(storage,`player/${currentUser.uid}`);
-    console.log(playerRef);
-    onDisconnect(playerRef).remove().catch((err) => {
-        if (err) {
-          console.error("could not establish onDisconnect event", err);
-        }
-      });
+    // let playerRef = ref(storage,`player/${playerId}`);
+    // console.log(playerRef);
+    // onDisconnect(playerRef).remove().catch((err) => {
+    //     if (err) {
+    //       console.error("could not establish onDisconnect event", err);
+    //     }
+    //   });
 
-    // playerRef.onDisconnect().remove();
-    // onDisconnect(playerRef).remove();
 
 
     // let playerId;
