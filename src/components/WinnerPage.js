93438f1d08a03bb1696ad1f7e8ffb4b48359bf6f
aaronub <wo-08@hotmail.com>
2022-10-12 14:59:31 -0400

    fixed warning

--- src/components/WinnerPage.js
+++ src/components/WinnerPage.js
@@ -12,7 +12,7 @@ const WinnerPage = ()=>{
 
     return (
         <>  
-            <img src='https://storybodyguard.files.wordpress.com/2011/02/winner-illustration1.jpg'/>
+            <img src='https://storybodyguard.files.wordpress.com/2011/02/winner-illustration1.jpg' alt='Winner Page'/>
             <h1>{winner} is the winner and got 5 stars!!!</h1>
             <h3>Not bad {player2}, you got {player2Points} points!</h3>
         </>
