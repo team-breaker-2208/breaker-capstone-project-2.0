d089c8e6bddd22e761308fba75d914f5c00a1090
aaronub <wo-08@hotmail.com>
2022-10-18 15:56:26 -0400

    Winner Page warning fixed

--- src/components/WinnerPage.js
+++ src/components/WinnerPage.js
@@ -24,32 +24,32 @@ const WinnerPage = ()=>{
     // deletegames()
 
 
-    // const location = useLocation();
-    // const gameId = location.state
+    const location = useLocation();
+    const gameId = location.state
     // console.log("Game ID is: ", gameId)
 
-    // useEffect(() => {
-    //     const getGameInfo = async() => {
-    //         const currentGameRef = doc(db, "cookieClickerGames", gameId)
-    //         const currentGameSnap = await getDoc(currentGameRef)
-    //         if (currentGameSnap.exists()) {
-    //             setLosers(currentGameSnap.data().losers)
-    //             setWinner(currentGameSnap.data().winner)
-    //             console.log("losers are: ", losers)
-    //             console.log("winner is: ", winner)
-    //         }
-    //     }
+    useEffect(() => {
+        const getGameInfo = async() => {
+            const currentGameRef = doc(db, "cookieClickerGames", gameId)
+            const currentGameSnap = await getDoc(currentGameRef)
+            if (currentGameSnap.exists()) {
+                setLosers(currentGameSnap.data().losers)
+                setWinner(currentGameSnap.data().winner)
+                console.log("losers are: ", losers)
+                console.log("winner is: ", winner)
+            }
+        }
 
         
 
-    //     getGameInfo()
-    //     // eslint-disable-next-line react-hooks/exhaustive-deps
-    // },[])
+        getGameInfo()
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    },[])
     
 
     return (
         <>  
-            {/* <div className="winner-title">
+            <div className="winner-title">
                 <h1 className="title-word title-word-1">{winner} is the winner, got 5 stars !!!</h1>
                 <div className="title-word-2">
                     {losers.map(loser => {
@@ -58,15 +58,15 @@ const WinnerPage = ()=>{
                         )
                     })}
                 </div>
-            </div> */}
-            <div className="winner-title">
+            </div>
+            {/* <div className="winner-title">
                 <h1 className="title-word title-word-1">winner is the winner, got 5 stars !!!</h1>
                 <div className="title-word-2">
                     <h3 className='losers' key='1'>aaa got 1 points!</h3>
                     <h3 className='losers' key='2'>bbb got 1 points!</h3>
                     <h3 className='losers' key='3'>ccc got 1 points!</h3>
                 </div>
-            </div>
+            </div> */}
             
             {/* <img src='https://storybodyguard.files.wordpress.com/2011/02/winner-illustration1.jpg' alt='Winner Page'/>
             <h1>{winner} is the winner and got 5 stars!!!</h1>
